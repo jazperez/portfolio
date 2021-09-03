f86f6f96d12e0deda6c1e6cb6350e25e62c4add4
jazperez <jazmin.perez.634@my.csun.edu>
2021-09-03 14:16:40 -0700

    Disable experience link

--- src/App.js
+++ src/App.js
@@ -9,9 +9,9 @@ function App() {
         <div className="welcome-content">
           <span className="title">Hello!</span>
           <span className="menu">
-            <a href="experience" class="button blue-background">Experience</a>
-            <a href="https://github.com/jazperez" class="button purple-background">Github</a>
-            <a href="https://www.linkedin.com/in/jazmin-perez24/" class="button pink-background">Linkedin</a>
+            <a href="experience" class="button blue-background disabled" alt="Experience">Experience</a>
+            <a href="https://github.com/jazperez" class="button purple-background" alt="Github profile">Github</a>
+            <a href="https://www.linkedin.com/in/jazmin-perez24/" class="button pink-background" alt="Linkedin profile">Linkedin</a>
           </span>
           <p className="bio">I am Jazmin Perez and I am a Software Engineer. I recently graduated from CSU Northridge with a B.S. in Computer Science and a minor in Chicanx Studies. </p>
           <p className="bio">I am an active member of the Society of Women Engineers, Society of Hispanic Engineers and Girls Who Code.</p>
