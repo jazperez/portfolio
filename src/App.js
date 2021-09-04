221ef4ea775a060bfe4aaeb016b0a28f23ca2d50
jazperez <jazmin.perez.634@my.csun.edu>
2021-09-04 16:15:05 -0700

    Add itype greeting

--- src/App.js
+++ src/App.js
@@ -1,19 +1,34 @@
 import './App.scss';
 import profilePicture from './JazPortfolioPic.png';
+import { init } from "ityped";
+import { useEffect, useRef } from "react";
 
 function App() {
+  const textRef = useRef();
+
+  useEffect(() => {
+    init(textRef.current, {
+      showCursor: true,
+      backDelay: 2000,
+      backSpeed: 80,
+      strings: ["Hello!", "¡Hola!", "¡Niltze!"],
+    });
+  }, []);
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={profilePicture} className="profile-pic" alt="Jaz's profile" />
         <div className="welcome-content">
-          <span className="title">Hello!</span>
+          <span className="title">
+            <span className="greeting" ref={textRef}></span>
+          </span>
           <span className="menu">
             <a href="experience" className="button blue-background disabled" alt="Experience">Experience</a>
             <a href="https://github.com/jazperez" className="button purple-background" alt="Github profile">Github</a>
             <a href="https://www.linkedin.com/in/jazmin-perez24/" className="button pink-background" alt="Linkedin profile">Linkedin</a>
           </span>
-          <p className="bio">I am Jazmin Perez and I am a Software Engineer. I recently graduated from CSU Northridge with a B.S. in Computer Science and a minor in Chicanx Studies. </p>
+          <p className="bio">I am Jazmin Perez and I am a Software Engineer. <br></br>I recently graduated from CSU Northridge with a B.S. in Computer Science and a minor in Chicanx Studies. </p>
           <p className="bio">I am an active member of the Society of Women Engineers, Society of Hispanic Engineers and Girls Who Code.</p>
           <p className="bio">I thought it would be fun to learn React, so I created this portfolio. Enjoy!</p>
         </div>
