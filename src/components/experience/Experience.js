ae25aba0c3cb2efb7ad776861dbe1d7122511011
jazperez <jazmin.perez.634@my.csun.edu>
2021-09-04 17:19:48 -0700

    Add experience and navbar component

---
+++ src/components/experience/Experience.js
@@ -0,0 +1,10 @@
+import './experience.scss';
+
+function Experience() {
+    return (
+    <div className="App">
+    </div>
+    );
+}
+
+export default Experience;
---
+++ src/components/navbar/Navbar.js
@@ -0,0 +1,10 @@
+import './navbar.scss';
+
+function Navbar() {
+    return (
+    <div className="App">
+    </div>
+    );
+}
+
+export default Navbar;
