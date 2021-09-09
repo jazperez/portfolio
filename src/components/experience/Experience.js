2f38feb1993fde29693a0c1d9bdc096cc0a73ec0
jazperez <jazmin.perez.634@my.csun.edu>
2021-09-08 17:19:52 -0700

    Add react router, move home out of app.js and make new empty components

--- src/components/experience/Experience.js
+++ src/components/experience/Experience.js
@@ -2,7 +2,8 @@ import './experience.scss';
 
 function Experience() {
     return (
-    <div className="App">
+    <div>
+        <h1 className="white">Page under construction, please come back later 😊</h1>
     </div>
     );
 }
--- src/components/navbar/Navbar.js
+++ src/components/navbar/Navbar.js
@@ -1,6 +1,11 @@
 import './navbar.scss';
 
 function Navbar() {
+
+    constructor(props) {
+        super(props)
+        this.state = { matches: window.matchMedia("(min-width: 768px)").matches };
+    }
     return (
     <div className="App">
     </div>
