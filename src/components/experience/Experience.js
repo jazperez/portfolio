0f43122c6f6e3a069a887f86d7a226e59cb37b1d
jazperez <jazmin.perez.634@my.csun.edu>
2021-09-14 12:53:56 -0700

    Fix media watcher logic

--- src/components/experience/Experience.js
+++ src/components/experience/Experience.js
@@ -2,6 +2,23 @@ import './experience.scss';
 import Navbar from '../navbar/Navbar';
 
 function Experience() {
+    const roles = [
+        { id: 1, 
+        title: 'Software Engineering Intern', 
+        company: 'PiñataFarms', 
+        date: 'Sep 2020 - Dec 2020', 
+        location: 'Remote', 
+        team: 'Engineering', 
+        description:'Built various web clients, both for internal infrastructure and consumer facing products using Vue.js, GraphQL and AWS. Designed and tested accessible and high quality UI/UX using Figma. Debugged and updated existing features.'},
+        { id: 2, 
+        title: 'Software Engineering Intern', 
+        company: 'Uber', 
+        date: 'Sep 2020 - Dec 2020', 
+        location: 'Remote', 
+        team: 'Driving Safety', 
+        description:'Developed a feature in Scala on an existing platform to process telematics data aimed towards building products to ensure the safety of users on the Uber platform. Queried and analyzed data using SQL and Jupyter Notebook.'},
+    ];
+
     return (
     <div>
         <Navbar />
